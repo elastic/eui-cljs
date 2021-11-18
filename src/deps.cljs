622aa55748637ff3bd7dd58946dd23d57cc9c783
Ryan Haywood <ryanhaywoodj@gmail.com>
2021-11-18 13:28:14 -0500

    Add missing files

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,1 +1,7 @@
-{:npm-deps {"@elastic/eui" "41.2.0"}}
+{:npm-deps
+ {"@elastic/eui" "41.2.0",
+  "@elastic/datemath" "^5.0.3",
+  "classnames" "^2.2.6",
+  "moment" "^2.27.0",
+  "prop-types" "^15.6.0",
+  "typescript" "4.0.5"}}
