78d50996ac29bf80d1fa434ac4b7c82dedc7725f
Ryan Haywood <ryanhaywoodj@gmail.com>
2022-08-17 15:57:22 -0400

    Update the example to use the eui provider for theming

--- examples/sample-app/src/sample_app/core.cljs
+++ examples/sample-app/src/sample_app/core.cljs
@@ -10,6 +10,7 @@
             [eui.page-content :refer [EuiPageContent]]
             [eui.page-content-body :refer [EuiPageContentBody]]
             [eui.page-header :refer [EuiPageHeader]]
+            [eui.provider :refer [EuiProvider]]
             [eui.text :refer [EuiText]]
             [eui.theme :as theme]
             [reagent.core :as r]
@@ -29,26 +30,27 @@
   (let [current-theme (:theme @app-state)
         page-title (:page-title @app-state)
         next-theme (theme/next-theme current-theme)]
-    [:> EuiPage {:paddingSize "none"}
-     [:> EuiPageBody {:restrictWidth true}
-      [:> EuiPageHeader {;; for this to work we need to register the icon with EUI down below
-                         :iconType "logoElastic"
-                         :pageTitle page-title
-                         :rightSideItems [(r/as-element ;; When passing components to EUI through props (not children)
-                                                      ;; use r/as-element to convert the Clojurescript to callable React
-                                           [:> EuiButton {:onClick (fn [_]
-                                                                     (swap! app-state (fn [state]
-                                                                                        (assoc state :theme next-theme)))
-                                                                     (theme/set-theme! next-theme))}
-                                            (str "Toggle theme: " (name next-theme))])]
-                         :paddingSize "l"}]
-      [:> EuiPageContent
-       [:> EuiPageContentBody
-        [title-form]
-        [:> EuiHorizontalRule]
-        [:> EuiText
-         [:h3 "More info"]
-         [:p "Children passed to EUI component are automatically converted thanks to Reagent interop `:>`, but passing components through props should be converted with `(r/as-element)` first :)"]]]]]]))
+    [:> EuiProvider {:colorMode current-theme}
+     [:> EuiPage {:paddingSize "none"}
+      [:> EuiPageBody {:restrictWidth true}
+       [:> EuiPageHeader {;; for this to work we need to register the icon with EUI down below
+                          :iconType "logoElastic"
+                          :pageTitle page-title
+                          :rightSideItems [(r/as-element ;; When passing components to EUI through props (not children)
+                                            ;; use r/as-element to convert the Clojurescript to callable React
+                                            [:> EuiButton {:onClick (fn [_]
+                                                                      (swap! app-state (fn [state]
+                                                                                         (assoc state :theme next-theme)))
+                                                                      (theme/set-theme! next-theme))}
+                                             (str "Toggle theme: " (name next-theme))])]
+                          :paddingSize "l"}]
+       [:> EuiPageContent
+        [:> EuiPageContentBody
+         [title-form]
+         [:> EuiHorizontalRule]
+         [:> EuiText
+          [:h3 "More info"]
+          [:p "Children passed to EUI component are automatically converted thanks to Reagent interop `:>`, but passing components through props should be converted with `(r/as-element)` first :)"]]]]]]]))
 
 (defn init []
   ;; Work around needed because the Closure compiler does not
