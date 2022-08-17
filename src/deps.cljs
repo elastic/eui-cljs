fe5fbe57f48cf27034139a408a55eaca0d9ed94a
Ryan Haywood <ryanhaywoodj@gmail.com>
2022-08-17 09:09:27 -0400

    Add @emotion/react to the peer dependencies

--- src/deps.cljs
+++ src/deps.cljs
@@ -1,7 +1,11 @@
 {:npm-deps
- {"@elastic/eui" "63.0.0",
-  "@elastic/datemath" "^5.0.3",
-  "classnames" "^2.2.6",
-  "moment" "^2.27.0",
-  "prop-types" "^15.6.0",
-  "typescript" "4.0.5"}}
+ {"@elastic/datemath" "^5.0.2",
+  "@emotion/react" "11.x",
+  "@emotion/cache" "11.x",
+  "@types/react" "^16.9 || ^17.0",
+  "@types/react-dom" "^16.9 || ^17.0",
+  "moment" "^2.13.0",
+  "prop-types" "^15.5.0",
+  "react" "^16.12 || ^17.0",
+  "react-dom" "^16.12 || ^17.0",
+  "typescript" "~4.5.3"}}
